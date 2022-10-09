ac4b9132fede9f8449e5049dda8b77eaea564904
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-10-09 15:02:49 +0300

    quaniity edit vendor products table

--- platform/plugins/marketplace/resources/views/themes/dashboard/table/actions.blade.php
+++ platform/plugins/marketplace/resources/views/themes/dashboard/table/actions.blade.php
@@ -11,7 +11,9 @@
             <i class="fa fa-trash"></i>
         </a>
     @endif
+    @if(Request::segment(2) == 'products')
     <a class="btn btn-icon btn-sm btn-info editQuantity" data-href="{{route('marketplace.vendor.products.change_quantity')}}" data-id="{{$item->id}}" data-quantity="{{$item->quantity}}" title="{{trans('core/base::tables.edit_quantity')}}">
         <i class="fa fa-cubes" style="color:black;font-size:1.6rem;"></i>
     </a>
+    @endif
 </div>
