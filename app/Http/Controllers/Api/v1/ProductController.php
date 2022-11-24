74644bd85164150cd7b5af2eb7a7a33b5cd3c91b
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-11-24 16:08:43 +0200

    up

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -160,6 +160,9 @@ class ProductController extends Controller
         //         }
         //     }
         // });
+
+        ini_set('max_execution_time' , 900);
+        
         dd(Product::query()->whereHas('translations' , function($trans){
             $trans->whereNull('content');
         })->count()
