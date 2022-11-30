d27011524bd9fdd50a64bfa9b1506b0ea70e4cbc
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-11-30 19:35:06 +0200

    up

---
+++ database/migrations/2022_11_30_173439_change_house_no_column_in_ec_order_addresses.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeHouseNoColumnInEcOrderAddresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ec_order_addresses', function (Blueprint $table) {
+            $table->string('house_no')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ec_order_addresses', function (Blueprint $table) {
+            $table->dropColumn(['house_no']);
+            
+        });
+    }
+}
