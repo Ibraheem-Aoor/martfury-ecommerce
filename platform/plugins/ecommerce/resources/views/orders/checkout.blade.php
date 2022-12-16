197f695a10145bc30d8c216528cbb3b04990356a
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-12-16 18:31:22 +0200

    update ideal

--- platform/plugins/ecommerce/resources/views/orders/checkout.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/checkout.blade.php
@@ -319,6 +319,56 @@
 
 
     <script>
+        window.addEventListener('load', function() {
+
+            // Loop over each funding source/payment method
+
+            paypal.Marks({
+                fundingSource: paypal.FUNDING.IDEAL
+            }).render('#ideal-mark');
+            let button;
+            button = paypal.PaymentFields({
+                fundingSource: paypal.FUNDING.IDEAL,
+                style: {
+                    // style object (optional)
+                },
+            })
+
+            paypal.Buttons({
+                fundingSource: paypal.FUNDING.IDEAL,
+                style: {
+                    label: "pay",
+                },
+                createOrder(data, actions) {
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                currency: 'EUR',
+                                value: "{{ (float) $total_amount }}",
+                            }
+                        }]
+                    });
+                },
+                onApprove(data, actions) {
+                    return actions.order.capture().then(function(orderData) {
+                        $('input[type="radio"][name="payment_method"]').val('iDEAL');
+                        $('#checkout-btn-custom').click();
+                        $('#checkout-btn-custom').attr('disabled', 'disabled');
+                    });
+                },
+                onCancel(data, actions) {
+                    // console.log(`Order Canceled - ID: ${data.orderID}`);
+                    toastr.error("{{ __('Payment failed!') }}");
+                },
+                onError(err) {
+                    toastr.error("{{ __('Payment failed!') }}");
+                }
+            }).render("#ideal-btn");
+            button.close();
+            button.render('#ideal-container');
+        });
+    </script>
+    <script>
         $(document).on('click', '#payment_ideal', function() {
             $('#checkout-btn-custom').hide();
         });
