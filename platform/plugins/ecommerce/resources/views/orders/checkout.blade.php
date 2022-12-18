f3883e15be937b1a755c28723f5919d48722131b
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-12-18 03:23:43 +0200

    up

--- platform/plugins/ecommerce/resources/views/orders/checkout.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/checkout.blade.php
@@ -200,28 +200,33 @@
                                         'name' => null,
                                     ]) !!}
                                     @foreach ($paynl_payment_methods as $method)
-                                    <li class="list-group-item">
-                                        <input class="magic-radio js_payment_method" type="radio" name="payment_method"
-                                            value="{{ @$method['id'] }}" data-bs-toggle="collapse" data-bs-target=".payment_{{@$method['brand']['id'] }}_wrap"
-                                            data-parent=".list_payment_method">
-                                        <label class="text-start">
-                                            <img src="{{ asset('payment-images-master/' . @$method['brand']['image']) }}" width="100" alt="">
-                                            {{ @$method['brand']['name'] }}</label>
-                                        <div class="payment_{{@$method['brand']['id'] }}_wrap payment_collapse_wrap show" style="padding: 15px 0;" >
-                                            <p>
-                                                {{ @$method['brand']['public_description'] }}
-                                            </p>
-                                            @isset($method['banks'])
-                                                <select name="method_bank" class="form-control">
-                                                    <option value="">--SELECT BANK--</option>
-                                                    @foreach (@$method['banks'] as $bank)
-                                                    <option value="{{@$bank['id']}}">{{@$bank['visibleName']}}</option>
-                                                    @endforeach
-                                                </select>
-                                            @endisset
-                                        </div>
-                                    </li>
-                                @endforeach
+                                        <li class="list-group-item">
+                                            <input class="magic-radio js_payment_method" type="radio"
+                                                name="payment_method" value="{{ @$method['id'] }}"
+                                                data-bs-toggle="collapse"
+                                                data-bs-target=".payment_{{ @$method['brand']['id'] }}_wrap"
+                                                data-parent=".list_payment_method">
+                                            <label class="text-start">
+                                                <img src="{{ asset('payment-images-master/' . @$method['brand']['image']) }}"
+                                                    width="100" alt="">
+                                                {{ @$method['brand']['name'] }}</label>
+                                            <div class="payment_{{ @$method['brand']['id'] }}_wrap payment_collapse_wrap show"
+                                                style="padding: 15px 0;">
+                                                <p>
+                                                    {{ @$method['brand']['public_description'] }}
+                                                </p>
+                                                {{-- @isset($method['banks'])
+                                                    <select name="method_bank" class="form-control">
+                                                        <option value="">--SELECT BANK--</option>
+                                                        @foreach (@$method['banks'] as $bank)
+                                                            <option value="{{ @$bank['id'] }}">{{ @$bank['visibleName'] }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                @endisset --}}
+                                            </div>
+                                        </li>
+                                    @endforeach
 
                                     @if (setting('payment_cod_status') == 1)
                                         <li class="list-group-item">
@@ -332,3 +337,19 @@
 
 
 @stop
+
+
+
+<li class="list-group-item">
+    <input class="magic-radio js_payment_method" type="radio" name="payment_method" id="payment_paypal"
+        value="paypal" data-bs-toggle="collapse" data-bs-target=".payment_paypal_wrap"
+        data-parent=".list_payment_method" aria-expanded="true">
+    <label for="payment_paypal" class="text-start">Pay online via PayPal</label>
+    <div class="payment_paypal_wrap payment_collapse_wrap" style="padding: 15px 0px;">
+        Payment with PayPal
+
+
+    </div>
+</li>
+
+
