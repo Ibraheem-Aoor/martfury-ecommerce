53b9369b5441a77336decc9e34f5ff6d3716b1df
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-12-18 23:17:46 +0200

    up

--- platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
@@ -594,7 +594,6 @@ class PublicCheckoutController
         HandleRemoveCouponService $removeCouponService,
         HandleApplyPromotionsService $handleApplyPromotionsService
     ) {
-        dd($request);
         if (!EcommerceHelper::isCartEnabled()) {
             abort(404);
         }
